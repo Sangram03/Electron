6e2b69c48716d63845a5c3587ba0ea225ea65b16
Sangram Das <sangramraju143@gmail.com>
2026-05-31 21:03:03 +0530

    added the electron app folder correctly

---
+++ src/electron/main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    mainWindow.loadFile(
+      path.join(app.getAppPath(), "dist-react", "index.html")
+    );
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
